598b473db2a699d09a28c0d368d31cd857677775
Mils <mils@prontomarketing.com>
2018-11-02 13:18:58 +0700

    Refactor :koala:

--- fabfile.py
+++ fabfile.py
@@ -2,15 +2,9 @@ from fabric import task
 from fabric import Connection
 from fabric import SerialGroup as Group
 
-#pool = Group('192.168.1.10', '192.168.1.20')
-#pool = Group('vagrant@192.168.1.10', 'vagrant@192.168.1.20')
-
-#ca = Connection(user='vagrant', host='192.168.1.10',port='22', connect_kwargs={"password":'vagrant'})
-
 #@task
 def install_package(c):
     c.run('sudo apt-get update')
-
 
 #@task
 def install_nginx(c):
@@ -18,7 +12,6 @@ def install_nginx(c):
 
 @task
 def run_all(c):
-    for host in ('vagrant@192.168.1.10', 'vagrant@192.168.1.20'):
-        ca = Connection(host=host, port='22', connect_kwargs={"password":'vagrant'})
-        install_package(ca)
-        install_nginx(ca)
+    pool = Group('vagrant@192.168.1.10', 'vagrant@192.168.1.20', connect_kwargs={"password":'vagrant'})
+    install_package(pool)
+    install_nginx(pool)
